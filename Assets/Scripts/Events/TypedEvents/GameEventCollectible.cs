bbedce49fde2ff6c624dc50f0a1a45ea377b9b04
Maxime Diversay <m.diversay@gmail.com>
2023-07-18 20:05:46 +0200

    Added URP Added Collectible mechanic Imported some new Particles

---
+++ Assets/Scripts/Events/TypedEvents/GameEventCollectible.cs
@@ -0,0 +1,9 @@
+﻿using Assets.Scripts.ScriptableObjects.Items;
+using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Events.TypedEvents
+{
+    [CreateAssetMenu(fileName = "GameEvent with Collectible Param", menuName = "GameEvent/Collectible", order = 3)]
+    public class GameEventCollectible : GameEvent<CollectibleSO> { }
+}
---
+++ Assets/Scripts/ScriptableObjects/Items/Collectible/XpCollectible.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Assets.Scripts.ScriptableObjects.Items.Weapons
+{
+    [CreateAssetMenu(fileName = "Collectible", menuName = "Collectible/Basic", order = 1)]
+    public class XpCollectible : CollectibleSO
+    {
+        public override void Collect()
+        {
+            Debug.Log("COLLECTED");
+        }
+    }
+}
