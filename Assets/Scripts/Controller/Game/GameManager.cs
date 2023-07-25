135240f22cf51ee3e75d7dccd9c83bc658ed4c89
Jules Grateau <jules.grateau@gmail.com>
2023-07-25 23:25:39 +0900

    Add TMP + Main Menu & Pause Menu

---
+++ Assets/Scripts/Controller/Game/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Controller.Game
+{
+    public class GameManager : MonoBehaviour
+    {
+        public void OnPause()
+        {
+            Time.timeScale = 0;
+        }
+
+        public void OnUnpause()
+        {
+            Time.timeScale = 1;
+        }
+    }
+}
