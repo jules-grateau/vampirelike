9f3ab9ef8dc515916a00289500c2a518ea9f7cd1
Maxime Diversay <m.diversay@gmail.com>
2023-08-06 12:35:54 +0200

    Part change

---
+++ Assets/Scripts/ScriptableObjects/Items/Collectible/KeyCollectibleSO.cs
@@ -0,0 +1,19 @@
+﻿using Assets.Scripts.Controller.Collectible;
+using Assets.Scripts.Events.TypedEvents;
+using UnityEngine;
+
+namespace Assets.Scripts.ScriptableObjects.Items
+{
+    [CreateAssetMenu(fileName = "Collectible", menuName = "Collectible/Key", order = 1)]
+    public class KeyCollectibleSO : CollectibleSO<KeyCollectible>
+    {
+        public override KeyCollectible GetCollectible(Vector3 position)
+        {
+            KeyCollectible keyCollectibleController = base.GetCollectible(position);
+            // TODO : add service to increment KeyId number
+            keyCollectibleController.KeyId = 0;
+
+            return keyCollectibleController;
+        }
+    }
+}
