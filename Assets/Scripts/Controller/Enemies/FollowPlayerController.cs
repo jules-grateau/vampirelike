305f5bdfa1d00184382537db6dbba3d3e4b989d0
Maxime Diversay <m.diversay@gmail.com>
2023-08-30 22:34:46 +0200

    Reworked enemy movement to use inheritance

--- Assets/Scripts/Controller/Enemies/FollowPlayerController.cs
+++ Assets/Scripts/Controller/Enemies/FollowPlayerController.cs
@@ -6,52 +6,18 @@ using UnityEngine;
 using Assets.Scripts.Types;
 
 [RequireComponent(typeof(Rigidbody2D))]
-public class FollowPlayerController : IEnemyMovement
+public class FollowPlayerController : MovementController
 {
-    [SerializeField]
-    private GameObject _player;
-    private Rigidbody2D _rigidbody;
-    private EnemyHealth _enemyHealth;
-
-    private bool _isFlipped = false;
-
     // Start is called before the first frame update
-    void Start()
+    protected override void Start()
     {
-        _player = GameObject.FindGameObjectWithTag("Player");
-        _rigidbody = GetComponent<Rigidbody2D>();
-        _enemyHealth = GetComponent<EnemyHealth>();
+        base.Start();
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override Vector3 ComputeDirection()
     {
-        if (!_player) return;
-        if (_enemyHealth.hasStun())
-        {
-            _rigidbody.velocity = Vector2.zero;
-            return;
-        }
-        
-         float modifiedSpeed = Speed * _enemyHealth.getSlowValue();
-
         Vector3 direction = _player.transform.position - transform.position;
         direction -= direction.normalized * 0.5f;
-
-        if (Mathf.Abs(direction.magnitude) < 0.1)
-        {
-            _rigidbody.velocity = Vector2.zero;
-        }
-        else
-        {
-            _rigidbody.velocity = direction.normalized * modifiedSpeed;
-        }
-        bool needToFlip = direction.normalized.x < 0;
-
-        if (needToFlip != _isFlipped)
-        {
-            transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
-            _isFlipped = needToFlip;
-        }
+        return direction;
     }
 }
